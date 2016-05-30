0061e04416ba612b7303e79cf8543ff2e4853100
gabrieltkgp <gabrieltkgp@gmail.com>
2016-05-29 23:25:59 -0300

    correcao de bug

--- classes/components.php
+++ classes/components.php
@@ -4,68 +4,76 @@ include_once("tools.php");
 class Components{
 
 	private $oTools;
-	private $oConn;
-	private $oStmt;
 
 	function __construct(){
 		$this->oTools = new Tools();
-		$this->oConn = $this->oTools->getConn();
 	}
 
 	private function queryEstados(){
-		$sSql = "SELECT id, sigla, nome FROM estados ORDER BY nome";
-	   	$this->oStmt = $this->oConn->prepare($sSql);	
-		$this->oStmt->execute();
+		return "SELECT id, sigla, nome FROM estados ORDER BY nome";
 	}
 	
 	public function createEstadosDropDown(){
-		$this->queryEstados();
+		$oConn = $this->oTools->getConn();
 
-		$this->oStmt->store_result();
+		$oStmt = $oConn->prepare($this->queryEstados());	
+		
+		$oStmt->execute();
 
-      	$this->oStmt->bind_result($id, $sigla, $nome);
+		$oStmt->store_result();
+
+      	$oStmt->bind_result($id, $sigla, $nome);
 
 		//$oResult = $this->oStmt->get_result();
 
 		$sDropDown = "<select id='selection_estados' name='idEstado' class='campo'> ";
 		$sDropDown = $sDropDown . "<option value=''></option> ";
-		if($this->oStmt->num_rows > 0) {
-			while($this->oStmt->fetch()){
+		if($oStmt->num_rows > 0) {
+			while($oStmt->fetch()){
 				$sDropDown = $sDropDown . "<option value=$id>$nome</option> ";
 			}
 		}
 		$sDropDown = $sDropDown . "</select> ";
 
-		$this->oStmt->free_result();
+		$oStmt->free_result();
+
+		$oStmt->close();
+
+		$oConn->close();
 
 		return $sDropDown;
 	}
 
-	private function queryCidades($pnIdEstado){
-		$sSql = "SELECT id, nome, id_estado FROM cidades WHERE id_estado = ? ORDER BY nome";
-	   	$this->oStmt = $this->oConn->prepare($sSql);
-	   	$this->oStmt->bind_param('i', $pnIdEstado);	
-		$this->oStmt->execute();
+	private function queryCidades(){
+		return "SELECT id, nome, id_estado FROM cidades WHERE id_estado = ? ORDER BY nome";
 	}
 	
 	public function createCidadesDropDown($pnIdEstado){
-		$this->queryCidades($pnIdEstado);
+		$oConn = $this->oTools->getConn();
 
-		$this->oStmt->store_result();
+		$oStmt = $oConn->prepare($this->queryCidades());
+	   	$oStmt->bind_param('i', $pnIdEstado);	
+		$oStmt->execute();
 
-      	$this->oStmt->bind_result($id, $nome, $idEstado);
+		$oStmt->store_result();
+
+      	$oStmt->bind_result($id, $nome, $idEstado);
 
 		//$oResult = $this->oStmt->get_result();
 
 		$sDropDown = "<select id='selection_cidades' name='idCidade' class='campo'> ";
-		if($this->oStmt->num_rows > 0) {
-			while($this->oStmt->fetch()){
+		if($oStmt->num_rows > 0) {
+			while($oStmt->fetch()){
 				$sDropDown = $sDropDown . "<option value=$id>$nome</option> ";
 			}
 		}
 		$sDropDown = $sDropDown . "</select> ";
 
-		$this->oStmt->free_result();
+		$oStmt->free_result();
+
+		$oStmt->close();
+
+		$oConn->close();
 
 		return $sDropDown;
 	}
